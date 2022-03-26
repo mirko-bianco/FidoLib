c742d244864cb7826f5956548a4587c075ee074f
mirkomendrix <m.bianco@mendrix.nl>
2022-03-26 14:32:53 +0100

    Remove useless garbage collection

--- source/Collections/Fido.Collections.PerThreadDictionary.pas
+++ source/Collections/Fido.Collections.PerThreadDictionary.pas
@@ -27,10 +27,7 @@ interface
 uses
   {$ifdef MSWINDOWS}
   Windows,
-  Winapi.PsAPI,
-  Winapi.TlHelp32,
-  {$endif}
-  {$ifdef POSIX}
+  {$else}
   Posix.Pthread,
   {$endif}
   System.Threading,
@@ -47,16 +44,12 @@ type
   // This dictionary store one item per thread.
   TPerThreadDictionary<T> = class(TInterfacedObject, IPerXDictionary<T>)
   type
-    TThreadId = Integer;
+    TThreadId = Int64;
   strict private
     FLock: IReadWriteSync;
     FFactoryFunc: TFunc<T>;
-    FGarbageTask: ITask;
   protected
     FItems: IDictionary<TThreadId, T>;
-
-    function GetThreadsList: TArray<TThreadId>;
-    procedure PerformGarbageCollection;
   public
     constructor Create(const Ownership: TDictionaryOwnerships; const FactoryFunc: TFunc<T>); reintroduce;
     destructor Destroy; override;
@@ -71,94 +64,6 @@ implementation
 
 { TPerThreadDictionary<T> }
 
-{$ifdef MSWINDOWS}
-function TPerThreadDictionary<T>.GetThreadsList: TArray<TThreadId>;
-var
-  SnapProcHandle: THandle;
-  NextProc: Boolean;
-  TThreadEntry: TThreadEntry32;
-  PID: Cardinal;
-begin
-  PID := MainThreadID;
-  SnapProcHandle := CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
-  if not (SnapProcHandle <> INVALID_HANDLE_VALUE) then
-    Exit;
-
-  try
-    TThreadEntry.dwSize := SizeOf(TThreadEntry);
-    NextProc := Thread32First(SnapProcHandle, TThreadEntry);
-    while NextProc do
-    begin
-      if TThreadEntry.th32OwnerProcessID = PID then
-      begin
-        SetLength(Result, Length(Result) + 1);
-        Result[High(Result)] := TThreadEntry.th32ThreadID;
-      end;
-
-      NextProc := Thread32Next(SnapProcHandle, TThreadEntry);
-    end;
-  finally
-    CloseHandle(SnapProcHandle);
-  end;
-end;
-{$endif}
-{$ifdef POSIX}
-function TPerThreadDictionary<T>.GetThreadsList: TArray<TThreadId>;
-var
-  List: IList<TThreadId>;
-begin
-  List := TCollections.CreateList<TThreadId>;
-  TCollections.CreateList<string>(TDirectory.GetDirectories(Format('/Proc/%d/task', [MainThreadID]))).ForEach(
-    procedure(const Item: string)
-    var
-      Value: Integer;
-    begin
-      if TryStrToInt(Item, Value) then
-        List.Add(Value);
-    end);
-  Result := List.ToArray;
-end;
-{$endif}
-
-procedure TPerThreadDictionary<T>.PerformGarbageCollection;
-var
-  ThreadIds: TArray<TThreadId>;
-  LiveThreads: ISet<TThreadId>;
-  GarbageThreads: ISet<TThreadId>;
-begin
-  LiveThreads := TCollections.CreateSet<TThreadId>(GetThreadsList);
-  GarbageThreads := TCollections.CreateSet<TThreadId>;
-
-  if not Assigned(FLock) then
-    Exit;
-
-  FLock.BeginRead;
-  try
-    ThreadIds := FItems.Keys.ToArray;
-  finally
-    FLock.EndRead;
-  end;
-
-  TCollections.CreateList<TThreadId>(ThreadIds).ForEach(
-    procedure(const Id: TThreadId)
-    begin
-      if not LiveThreads.Contains(Id) and
-         (Id <> Integer(MainThreadID)) then
-        GarbageThreads.Add(Id);
-    end);
-
-  FLock.BeginWrite;
-  try
-    GarbageThreads.ForEach(
-      procedure(const Id: TThreadId)
-      begin
-        FItems.Remove(Id);
-      end);
-  finally
-    FLock.EndWrite;
-  end;
-end;
-
 constructor TPerThreadDictionary<T>.Create(
   const Ownership: TDictionaryOwnerships;
   const FactoryFunc: TFunc<T>);
@@ -166,31 +71,12 @@ begin
   Guard.CheckTrue(Assigned(FactoryFunc), 'FFactoryFunc');
   inherited Create;
   FLock := TMREWSync.Create;
-  FItems := Spring.Collections.TCollections.CreateDictionary<Integer, T>(Ownership);
+  FItems := Spring.Collections.TCollections.CreateDictionary<TThreadId, T>(Ownership);
   FFactoryFunc := FactoryFunc;
-  FGarbageTask := TTask.Run(
-    procedure
-    var
-      Index: Integer;
-    begin
-      while true do
-      begin
-        for Index := 1 to 10 do
-        begin
-          if TTask.CurrentTask.Status = TTaskStatus.Canceled then
-            Exit;
-          Sleep(100);
-        end;
-        PerformGarbageCollection;
-      end;
-    end);
 end;
 
 destructor TPerThreadDictionary<T>.Destroy;
 begin
-  FGarbageTask.Cancel;
-  Sleep(100);
-  PerformGarbageCollection;
   inherited;
 end;
 
