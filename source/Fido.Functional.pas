934bf5f471978df9beae5abe6282f94fbf38f544
mirkomendrix <m.bianco@mendrix.nl>
2022-07-04 22:49:49 +0200

    Better naming on OnFailure event

--- source/Fido.Functional.pas
+++ source/Fido.Functional.pas
@@ -42,7 +42,7 @@ type
 
   EFunctionalContext = class(EFidoException);
 
-  OnFailureEvent<T> = reference to function(const Value: TObject): T;
+  OnFailureEvent<T> = reference to function(const RaisedException: TObject): T;
   OnSuccessEvent<T> = reference to function(const Value: T): T;
 
   Context<T> = record
